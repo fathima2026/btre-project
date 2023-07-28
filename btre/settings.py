d567129494c02698b2e90768b3a1f5647fc0faa4
fathima <fathima2026@gmail.com>
2023-07-29 01:44:15 +0600

    edited admin page

--- btre/settings.py
+++ btre/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+print(BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -122,8 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
-STATICFILES_DIR = [
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]
 # Default primary key field type
--- listings/admin.py
+++ listings/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Listing
 # Register your models here.
-
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id','title','is_public','price','list_date','realtor')
+    list_display_links=('id','title')
+    list_filter = ('realtor',)
+    list_editable = ('is_public',)
+    search_fields=('title','description','address','city','zipcode','price')
+    list_per_page = 25
+admin.site.register(Listing,ListingAdmin)
